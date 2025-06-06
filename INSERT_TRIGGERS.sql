fe48011e9a57256da2b7f651c13b1da7e1b2b901
Sandro Flausino <sjflausino@gmail.com>
2025-06-06 17:19:16 -0300

    Add missing newline and reformat comments in INSERT_TRIGGERS.sql

--- INSERT_TRIGGERS.sql
+++ INSERT_TRIGGERS.sql
@@ -5,3 +5,7 @@ INSERT INTO monitores (aluno_id, curso_id, data_inicio) VALUES
 -- 2. Adiciona monitor inválido (sem certificado)
 INSERT INTO monitores (aluno_id, curso_id, data_inicio) VALUES
 (3, 5, CURRENT_DATE);
+
+-- 3. Verifica progresso e conclui curso
+INSERT INTO certificados (aluno_id, curso_id, codigo_validacao)
+VALUES (1, 1, 'CERT-TESTE-VAL');
