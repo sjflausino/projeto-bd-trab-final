ed50d184e4eacdee53e616720bb09be7c266d0cc
Gabs <gabriel_braga@id.uff.br>
2025-06-06 17:44:29 -0300

    mais ajustes e testes

--- init-db/INSERT.sql
+++ init-db/INSERT.sql
@@ -47,6 +47,8 @@ INSERT INTO inscricoes (aluno_id, curso_id, status) VALUES
 -- Progresso
 INSERT INTO progresso (aula_id, aluno_id, assistido, data_visualizacao) VALUES
 (1, 1, TRUE, CURRENT_DATE),
+(2, 1, TRUE, CURRENT_DATE),
+(3, 1, TRUE, CURRENT_DATE),
 (2, 1, FALSE, NULL),
 (3, 2, TRUE, CURRENT_DATE);
 
--- init-db/TRIGGERS.sql
+++ init-db/TRIGGERS.sql
@@ -22,32 +22,59 @@ FOR EACH ROW
 EXECUTE FUNCTION check_monitor();
 
 
+CREATE OR REPLACE FUNCTION check_nota(a_id integer,c_id integer) 
+RETURNS FLOAT 
+LANGUAGE plpgsql as $$
+DECLARE
+ 	nota float;
+BEGIN
+    nota := nota FROM avaliacoes WHERE aluno_id = a_id AND curso_id = c_id;
+	return nota;
+END;
+$$;
+
+
+
 CREATE OR REPLACE FUNCTION check_certificado()
 RETURNS TRIGGER AS $$
 DECLARE
-	aula_rec record;
-	assistido bool;
-    modulo_rec record;
-	nota float;
+    aula_rec RECORD;
+    _assistido BOOLEAN;
+    modulo_rec RECORD;
 BEGIN
- 
+    -- Loop pelos módulos do curso do aluno
+    FOR modulo_rec IN
+        SELECT * FROM modulos
+        WHERE curso_id = NEW.curso_id
+    LOOP
+        -- Loop pelas aulas do módulo
+        FOR aula_rec IN
+ 			SELECT * FROM aulas
+            WHERE modulo_id = modulo_rec.modulo_id
+        LOOP
+            -- Verifica se o aluno assistiu a aula
+            SELECT assistido INTO _assistido
+            FROM progresso
+            WHERE aula_id = aula_rec.aula_id AND aluno_id = NEW.aluno_id;
 
-    FOR modulo_rec in SELECT * FROM modulos WHERE aluno_id = NEW.aluno_id AND curso_id = NEW.curso_id loop
-		FOR aula_rec in SELECT * FROM Aulas where aluno_id = modelo_rec.aluno_id and modelo_id = modelo_rec.id LOOP
-			assistido := assistido FROM progresso where aula_id = aula_rec.aula_id and aluno_id = aula_rec.aluno_id;
-			IF not assistido Then 
-				RAISE EXCEPTION 'Aluno não assistiu todas as aulas.';
-			END IF;
-		END LOOP;	
-	END LOOP;
+            IF NOT _assistido THEN
+                RAISE EXCEPTION 'Aluno não assistiu todas as aulas do módulo %.', modulo_rec.modulo_id;
+            END IF;
+        END LOOP;
+    END LOOP;
 
-	nota := nota from avaliacoes where curso_id = new.curso_id and aluno_id = new.aluno_id;
-
-    IF nota < 6.0 THEN
-        RAISE EXCEPTION 'Aluno não possui nota para passar no curso.';
-   	ELSE
-		RAISE NOTICE 'Cetificado criado.';
-    	RETURN NEW;
-	END IF;
+    -- Verifica a nota mínima para certificado
+    IF check_nota(NEW.aluno_id, NEW.curso_id) < 6.0 THEN
+        RAISE EXCEPTION 'Aluno não possui nota suficiente para obter o certificado.';
+    ELSE
+        RAISE NOTICE 'Certificado criado com sucesso.';
+        RETURN NEW;
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER certificado_trigger
+BEFORE INSERT ON certificados
+FOR EACH ROW
+EXECUTE FUNCTION check_certificado();
+
